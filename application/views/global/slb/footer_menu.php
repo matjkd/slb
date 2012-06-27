fcf96383a51bd4a55651d88e8d32262d08a13b56
Mat Sadler <mat@redstudio.co.uk>
2012-06-27 16:03:32 +0000

    blam

---
+++ application/views/global/slb/footer_menu.php
@@ -0,0 +1,16 @@
+
+
+<div class="footertext" >
+    <div style="position:absolute; left:10px; top:15px;"> &nbsp;</div>
+    <div style="float:left; margin-top:23px; width: 400px; font-size: 11px;">
+   
+    Copyright &copy; <?= $config_company_name ?> 
+    <br/>
+    Website by: <a href="http://www.designco.org.uk">DesignCo</a>
+    </div>
+    
+    
+     <div style="float:left; width: 400px;">
+       &nbsp;
+    </div>
+</div>
---
+++ application/views/global/slb/menu.php
@@ -0,0 +1,24 @@
+
+<ul>
+    <li><?= anchor('/', 'Home') ?></li>
+    <li><?= anchor('/creditor', 'Creditor Services') ?></li>
+    <li><?= anchor('/debitor', ' Debitor Services') ?></li>
+     <li><?= anchor('/factoring-invoice-discounting', 'Factoring & Invoice Discounting') ?></li>
+
+    <li><?= anchor('/asset-finance', 'Asset Finance') ?></li>
+    
+    <li><?= anchor('/collections', 'Collections') ?></li>
+
+    <li><?= anchor('/contact', 'Contact Us') ?></li>
+
+
+    <?php
+    $is_logged_in = $this->session->userdata('is_logged_in');
+    $role = $this->session->userdata('role');
+    if ($is_logged_in != 0 || $role == 1) {
+
+        echo anchor('admin', 'Admin');
+    }
+    ?>
+
+</ul>
